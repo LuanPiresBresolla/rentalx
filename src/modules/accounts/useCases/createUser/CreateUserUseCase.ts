da7946695c495c2ac9cdc55ad9f8deb62b5b8ff9
Luan Bresolla <luanpires34@gmail.com>
2021-05-14 23:48:14 -0300

    ajustes para rodar a aplicacao

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,4 @@
-import { hash } from 'bcrypt';
+import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 
 import { User } from '@modules/accounts/infra/typeorm/entities/User';
